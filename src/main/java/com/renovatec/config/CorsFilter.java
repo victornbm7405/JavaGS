c3c432e1584f0ccc60517180f74409eb5eef31dc
victornm7405 <rm554557@fiap.com.br>
2024-11-22 14:51:52 -0300

    alterações feitas

--- src/main/java/com/renovatec/config/CorsFilter.java
+++ src/main/java/com/renovatec/config/CorsFilter.java
@@ -10,10 +10,10 @@ public class CorsFilter implements ContainerResponseFilter {
 
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) {
-        // Adiciona os cabeçalhos para permitir CORS
-        responseContext.getHeaders().add("Access-Control-Allow-Origin", "http://localhost:3000"); // Permite requisições do front
-        responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); // Métodos permitidos
-        responseContext.getHeaders().add("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization"); // Cabeçalhos permitidos
-        responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true"); // Permite cookies e credenciais (se necessário)
+        // Permite múltiplas origens
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+        responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        responseContext.getHeaders().add("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization");
+        responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
     }
 }
