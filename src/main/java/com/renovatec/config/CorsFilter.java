f17a0dff86c200ee4d6909f5ac678deaf44cbe58
victornm7405 <rm554557@fiap.com.br>
2024-11-22 13:55:46 -0300

    algumas coisas atualizadas

--- src/main/java/com/renovatec/config/CorsFilter.java
+++ src/main/java/com/renovatec/config/CorsFilter.java
@@ -4,16 +4,16 @@ import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerResponseContext;
 import jakarta.ws.rs.container.ContainerResponseFilter;
 import jakarta.ws.rs.ext.Provider;
+import java.io.IOException;
 
 @Provider
 public class CorsFilter implements ContainerResponseFilter {
+
     @Override
-    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) {
-        System.out.println("Interceptando requisição CORS para: " + requestContext.getUriInfo().getPath());
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
         responseContext.getHeaders().add("Access-Control-Allow-Origin", "http://localhost:3000");
-        responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
-        responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        responseContext.getHeaders().add("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization");
         responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
     }
 }
-
--- src/main/java/com/renovatec/controller/ProdutoController.java
+++ src/main/java/com/renovatec/controller/ProdutoController.java
@@ -2,56 +2,25 @@ package com.renovatec.controller;
 
 import com.renovatec.dao.ProdutoDAO;
 import com.renovatec.model.Produto;
-
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
+import java.util.List;
+
 @Path("/produtos")
 public class ProdutoController {
-
-    @DELETE
-    @Path("/{id}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response excluirProduto(@PathParam("id") int id) {
-        try {
-            produtoDAO.excluirProduto(id);
-            return Response.ok("Produto excluído com sucesso!").build();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-                    .entity("Erro ao excluir produto.")
-                    .build();
-        }
-    }
-
-
-    @PUT
-    @Path("/{id}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response atualizarProduto(@PathParam("id") int id, Produto produto) {
-        try {
-            produto.setId(id);
-            produtoDAO.atualizarProduto(produto);
-            return Response.ok("Produto atualizado com sucesso!").build();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-                    .entity("Erro ao atualizar produto.")
-                    .build();
-        }
-    }
-
-
     private ProdutoDAO produtoDAO = new ProdutoDAO();
 
-    // GET - Listar Produtos (já existente)
+    // GET - Listar Produtos
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response listarProdutos() {
+        System.out.println("Recebida requisição GET para listar produtos");
         try {
-            return Response.ok(produtoDAO.listarProdutos()).build();
+            List<Produto> produtos = produtoDAO.listarProdutos();
+            System.out.println("Produtos encontrados: " + produtos);
+            return Response.ok(produtos).build();
         } catch (Exception e) {
             e.printStackTrace();
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
@@ -65,6 +34,7 @@ public class ProdutoController {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response cadastrarProduto(Produto produto) {
+        System.out.println("Recebida requisição POST para cadastrar produto: " + produto);
         try {
             produtoDAO.cadastrarProduto(produto);
             return Response.status(Response.Status.CREATED)
@@ -75,7 +45,54 @@ public class ProdutoController {
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
                     .entity("Erro ao cadastrar produto.")
                     .build();
-
         }
     }
+
+    // PUT - Atualizar Produto
+    @PUT
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response atualizarProduto(@PathParam("id") int id, Produto produto) {
+        System.out.println("Recebida requisição PUT para atualizar produto com ID: " + id);
+        try {
+            produto.setId(id);
+            produtoDAO.atualizarProduto(produto);
+            return Response.ok("Produto atualizado com sucesso!").build();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity("Erro ao atualizar produto.")
+                    .build();
+        }
+    }
+
+    // DELETE - Excluir Produto
+    @DELETE
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response excluirProduto(@PathParam("id") int id) {
+        System.out.println("Recebida requisição DELETE para excluir produto com ID: " + id);
+        try {
+            produtoDAO.excluirProduto(id);
+            return Response.ok("Produto excluído com sucesso!").build();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity("Erro ao excluir produto.")
+                    .build();
+        }
+    }
+
+    // OPTIONS - Pré-flight para CORS
+    @OPTIONS
+    @Path("{path:.*}")
+    public Response handleOptions() {
+        return Response.ok()
+                .header("Access-Control-Allow-Origin", "http://localhost:3000")
+                .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+                .header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization")
+                .header("Access-Control-Allow-Credentials", "true")
+                .build();
+}
 }
